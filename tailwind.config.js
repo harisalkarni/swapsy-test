2d7026b8a4ee2121b497d0db43fa08ad2670b0d0
Su Diddo <su.diddo@gmail.com>
2022-06-27 12:06:23 +0800

    fix background color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'black': '#000',
-      'secondaryblack': '#0E0E0E',
-      'erie-black': '#181818',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'tea-green': '#D4FAC9',
-      'second-tea-green': '#DBF9CD',
-      'ocean-blue': '#423AD0',
-      'second-ocean-blue': '#4131CB',
-      'dark-charcoal': '#303030'
-    },
     extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
       opacity: {
-         '10': '0.1',
-          '20': '0.2',
-          '98': '0.98',
-          '80': '0.80'
+        10: "0.1",
+        20: "0.2",
+        98: "0.98",
+        80: "0.80",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000",
+        secondaryblack: "#0E0E0E",
+        "erie-black": "#181818",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+        "tea-green": "#D4FAC9",
+        "second-tea-green": "#DBF9CD",
+        "ocean-blue": "#423AD0",
+        "second-ocean-blue": "#4131CB",
+        "dark-charcoal": "#303030",
+        "blue-purple": "#413AC8",
       },
     },
   },
