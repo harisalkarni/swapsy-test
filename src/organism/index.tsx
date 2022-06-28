3cd4209550bcf8990ae775156d79e0dcc49bb850
krisnasudiatmika <kresnasudiatmika@gmail.com>
2022-06-28 15:33:36 +0800

    add withdrawing funds

--- src/organism/index.tsx
+++ src/organism/index.tsx
@@ -3,12 +3,13 @@
 // import WrongNetwork from "../atom/WrongNetwork";
 // import Swap from "../molecule/Swap";
 // import SwapScreen from "../molecule/SwapScreen";
-import CancelingSwap from "../atom/CancelingSwap";
-import SwapLink from "../molecule/SwapLink";
+// import CancelingSwap from "../atom/CancelingSwap";
+// import SwapLink from "../molecule/SwapLink";
+import WithdrawingFunds from "../atom/WithdrawingProcess";
 const HomePage = () => {
   return (
     <div className="flex flex-1 bg-black bg-opacity-80 min-h-screen justify-center items-center">
-      <CancelingSwap />
+      <WithdrawingFunds />
     </div>
   );
 };
