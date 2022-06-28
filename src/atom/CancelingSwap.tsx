aab63cd15eb5b896ac5d6e990711b00693a4d113
krisnasudiatmika <kresnasudiatmika@gmail.com>
2022-06-28 15:27:28 +0800

    add canceling swap

---
+++ src/atom/CancelingSwap.tsx
@@ -0,0 +1,13 @@
+const CancelingSwap = () => {
+  return (
+    <div className="w-[294px] h-[485px]  rounded-[5px] pt-[88px] px-[33px] relative bg-erie-black flex flex-col items-center">
+      <div className="text-[20px] text-tea-green">Swap #9481</div>
+      <div className="w-[50px] h-[50px] mt-[92px] mb-[25px]">x</div>
+      <div className="w-[104px] text-[20px] text-white text-center">
+        Canceling Swap..
+      </div>
+    </div>
+  );
+};
+
+export default CancelingSwap;
--- src/organism/index.tsx
+++ src/organism/index.tsx
@@ -3,11 +3,12 @@
 // import WrongNetwork from "../atom/WrongNetwork";
 // import Swap from "../molecule/Swap";
 // import SwapScreen from "../molecule/SwapScreen";
+import CancelingSwap from "../atom/CancelingSwap";
 import SwapLink from "../molecule/SwapLink";
 const HomePage = () => {
   return (
     <div className="flex flex-1 bg-black bg-opacity-80 min-h-screen justify-center items-center">
-      <SwapLink text={"Expires in 2d 12h"} button={false} />
+      <CancelingSwap />
     </div>
   );
 };
