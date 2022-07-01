1cd251acb115b89548d035b7c87721f870e3f599
Su Diddo <su.diddo@gmail.com>
2022-07-01 20:32:37 +0800

    add scroll on the table

--- src/atom/TransactionTable.tsx
+++ src/atom/TransactionTable.tsx
@@ -35,61 +35,65 @@ function TransactionTable({ type }: Props) {
         {!isCancelled && <div className="w-1/6"></div>}
       </div>
 
-      {MySwapDummy.map(({ amountTo, amountFrom, coinFrom, coinTo, id }) => {
-        return (
-          <div className="w-full mb-[8px] bg-erie-black px-[9px] py-[10px] rounded-[5px]">
-            <div className="flex flex-row justify-between text-white items-center">
-              <div className="w-1/5">
-                <div className="text-[10px] text-tea-green">{id}</div>
-                {isOpen && <div className="text-[6px]">Expires in 1d 45m</div>}
-              </div>
-              <div className="flex flex-row items-center w-1/3">
-                <div className="mr-[5px]">
-                  <img
-                    src={getCoin(coinFrom).avatar}
-                    className="w-[21px] h-[21px]"
-                    alt="coin icon"
-                  />
+      <div className="h-[274px] overflow-y-scroll">
+        {MySwapDummy.map(({ amountTo, amountFrom, coinFrom, coinTo, id }) => {
+          return (
+            <div className="w-full mb-[8px] bg-erie-black px-[9px] py-[10px] rounded-[5px]">
+              <div className="flex flex-row justify-between text-white items-center">
+                <div className="w-1/5">
+                  <div className="text-[10px] text-tea-green">{id}</div>
+                  {isOpen && (
+                    <div className="text-[6px]">Expires in 1d 45m</div>
+                  )}
                 </div>
-                <div>
-                  <div className="text-[7px] text-tea-green">
-                    {getCoin(coinFrom).name}
+                <div className="flex flex-row items-center w-1/3">
+                  <div className="mr-[5px]">
+                    <img
+                      src={getCoin(coinFrom).avatar}
+                      className="w-[21px] h-[21px]"
+                      alt="coin icon"
+                    />
                   </div>
-                  <div className="text-[10px]">{amountFrom}</div>
-                </div>
-              </div>
-              <div className="flex flex-row items-center w-1/3">
-                <div className="mr-[5px]">
-                  <img
-                    src={getCoin(coinTo).avatar}
-                    className="w-[21px] h-[21px]"
-                    alt="coin icon"
-                  />
-                </div>
-                <div>
-                  <div className="text-[7px] text-tea-green">
-                    {getCoin(coinTo).name}
+                  <div>
+                    <div className="text-[7px] text-tea-green">
+                      {getCoin(coinFrom).name}
+                    </div>
+                    <div className="text-[10px]">{amountFrom}</div>
                   </div>
-                  <div className="text-[10px]">{amountTo}</div>
                 </div>
+                <div className="flex flex-row items-center w-1/3">
+                  <div className="mr-[5px]">
+                    <img
+                      src={getCoin(coinTo).avatar}
+                      className="w-[21px] h-[21px]"
+                      alt="coin icon"
+                    />
+                  </div>
+                  <div>
+                    <div className="text-[7px] text-tea-green">
+                      {getCoin(coinTo).name}
+                    </div>
+                    <div className="text-[10px]">{amountTo}</div>
+                  </div>
+                </div>
+                {!isCancelled && (
+                  <div className="w-1/6">
+                    <button
+                      className={`text-[7px] px-[9px] py-[6px] rounded-full ${
+                        isCompleted
+                          ? " border border-tea-green text-white"
+                          : "bg-tea-green text-black"
+                      } ${isCompleted && "px-3"}`}
+                    >
+                      {renderButtonLabel()}
+                    </button>
+                  </div>
+                )}
               </div>
-              {!isCancelled && (
-                <div className="w-1/6">
-                  <button
-                    className={`text-[7px] px-[9px] py-[6px] rounded-full ${
-                      isCompleted
-                        ? " border border-tea-green text-white"
-                        : "bg-tea-green text-black"
-                    } ${isCompleted && 'px-3'}`}
-                  >
-                    {renderButtonLabel()}
-                  </button>
-                </div>
-              )}
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
