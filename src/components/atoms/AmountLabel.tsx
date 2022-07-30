f4af744e2f3d3591ee64ce6b6b80f61e54ada451
krisnasudiatmika <kresnasudiatmika@gmail.com>
2022-07-30 09:02:22 +0800

    change input type

--- src/components/atoms/AmountLabel.tsx
+++ src/components/atoms/AmountLabel.tsx
@@ -1,4 +1,5 @@
 import React, { useState, Dispatch } from "react";
+import NumberFormat from "react-number-format";
 
 interface Props {
   amount: number;
@@ -10,42 +11,35 @@ interface Props {
 function AmountLabel({ amount, name, setAmount, error }: Props) {
   const [width, setWidth] = useState(
     amount.toString().length === 1
-      ? 1
+      ? 20
       : amount.toString().length === 0
-      ? 1
-      : amount.toString().length
+      ? 20
+      : amount.toString().length * 10
   );
 
   return (
     <div>
       <div className="flex h-[33px] w-[85px] flex-row items-end">
-        <input
-          type="text"
-          step="0.01"
-          value={amount}
-          onChange={(e) => {
-            if (e.target.validity.valid) {
-              console.log("kontole", e.target.value.length);
-              const amount = e.target.value;
-              const targetValue = parseFloat(amount) || 0;
-              setAmount(targetValue);
-              setWidth(
-                e.target.value.length === 1
-                  ? 1
-                  : e.target.value.length === 0
-                  ? 1
-                  : e.target.value.length
-              );
-            }
-          }}
-          pattern="^[0-9]*[.,]?[0-9]*$"
-          size={width}
-          placeholder="0.00"
-          className={`h-6 max-w-[200px] bg-transparent text-left text-[24px] text-white outline-none placeholder:text-white/30 md:h-7 md:text-[27px] ${
+        <NumberFormat
+          placeholder="0"
+          className={`h-6 max-w-[200px]  bg-transparent text-left text-[24px] text-white outline-none placeholder:text-white/30 md:h-7 md:text-[27px] ${
             error ? "text-red" : amount === 0 ? "text-white/30" : "text-white"
           }`}
+          value={amount}
+          style={{ width: width }}
+          onValueChange={(values) => {
+            const { formattedValue } = values;
+            const val = parseFloat(formattedValue);
+            setWidth(
+              formattedValue.length === 1
+                ? 20
+                : formattedValue.length === 0
+                ? 20
+                : formattedValue.length * 10
+            );
+            setAmount(val);
+          }}
         />
-
         <div
           className={`${
             error ? "text-red" : amount === 0 ? "text-white/30" : "text-white"
